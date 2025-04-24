08694d8210a3a5b3d14f831f8a46158ba20da3bd
notifrin <80722932+notifrin@users.noreply.github.com>
2025-04-23 22:59:37 -0700

    Update FrogSimulation.java

--- src/main/java/FrogSimulation.java
+++ src/main/java/FrogSimulation.java
@@ -15,13 +15,34 @@ public class FrogSimulation
 	
 	public boolean simulate()
 	{ 
-		/* to be implemented in part (a) */
+	int position = 0;
+for (int count = 0; count < maxHops; count++)
+{
+position += hopDistance();
+if (position >= goalDistance)
+{
+return true;
+}
+else if (position < 0)
+{
+return false;
+}
+}
+return false;
 
 	}
 	
 	public double runSimulations(int num)
 	{ 
-		/* to be implemented in part (b) */ 
+		int countSuccess = 0;
+for (int count = 0; count < num; count++)
+{
+if(simulate())
+{
+countSuccess++;
+}
+}
+return (double)countSuccess / num;
 
 	}
 	
